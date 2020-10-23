103d3dcf5a7c69e0b8d69f23a0b0289928535f0d
Dale.Yarborough <dale.yarborough@wholefoods.com>
2020-10-23 14:43:44 -0500

    Adding Link to daleyarborough.com

--- python/histograph.py
+++ python/histograph.py
@@ -2,29 +2,20 @@ import plotly.graph_objects as go
 import numpy as np
 import os
 
-# Create Scatter Plot
+# Change Scatter Plot To Histogram
 # Then Save Graph As Image To Images Folder
 
-np.random.seed(1)
-
-N = 100
-x = np.random.rand(N)
-y = np.random.rand(N)
-colors = np.random.rand(N)
-sz = np.random.rand(N) * 30
+x0 = np.random.randn(2000)
+x1 = np.random.randn(2000) + 1
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(
-    x=x,
-    y=y,
-    mode="markers",
-    marker=go.scatter.Marker(
-        size=sz,
-        color=colors,
-        opacity=0.6,
-        colorscale="Viridis"
-    )
-))
+fig.add_trace(go.Histogram(x=x0))
+fig.add_trace(go.Histogram(x=x1))
+
+# The two histograms are drawn on top of another
+fig.update_layout(barmode='stack')
+fig.show()
+
 
 if not os.path.exists("graphs"):
     os.mkdir("graphs")
